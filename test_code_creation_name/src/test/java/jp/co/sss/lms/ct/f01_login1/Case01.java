e921dcbdf14cb92109715e58dab5394534a3f70e
tsk <tsk@TSK-P-021>
2026-06-25 11:22:34 +0900

    ログイン機能のログイン画面表示テストケースを追加(ケース1) ログイン機能の認証失敗テストケースを追加（ケース2）

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,6 @@
 package jp.co.sss.lms.ct.f01_login1;
 
-import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,26 +9,40 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 /**
  * 結合テスト ログイン機能①
  * ケース01
  * @author holy
  */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース01 ログイン画面への遷移")
 public class Case01 {
 
+	@LocalServerPort
+	private int port;
+	
+	private static WebDriver driver;
+	
 	/** 前処理 */
 	@BeforeAll
 	static void before() {
-		createDriver();
+		driver = new ChromeDriver();
 	}
 
 	/** 後処理 */
 	@AfterAll
 	static void after() {
-		closeDriver();
+		if(driver != null) {
+			driver.quit();
+		}
 	}
 
 	@Test
@@ -36,6 +50,14 @@ public class Case01 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		driver.get("http://localhost:" + port + "/lms");
+		
+		WebElement loginidElement = driver.findElement(By.id("loginId"));
+		WebElement passwordElement = driver.findElement(By.id("password"));
+		
+		assertTrue(loginidElement.isDisplayed(), "ログインID入力欄が表示されていること");
+		assertTrue(passwordElement.isDisplayed(), "パスワード入力欄が表示されていること");
+		
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,6 @@
 package jp.co.sss.lms.ct.f01_login1;
 
-import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,26 +9,45 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 /**
  * 結合テスト ログイン機能①
  * ケース02
  * @author holy
  */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース02 受講生 ログイン 認証失敗")
 public class Case02 {
 
+	@LocalServerPort
+	private int port;
+	
+	private static WebDriver driver;
+	private static WebDriverWait wait;
+	
+	
 	/** 前処理 */
 	@BeforeAll
 	static void before() {
-		createDriver();
+		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
 	}
 
 	/** 後処理 */
 	@AfterAll
 	static void after() {
-		closeDriver();
+		if(driver != null) {
+			driver.quit();
+		}
 	}
 
 	@Test
@@ -36,6 +55,15 @@ public class Case02 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		driver.get("http://localhost:" + port + "/lms");
+		
+		//間違ったIDを入力
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginId"))).sendKeys("aaa");
+		driver.findElement(By.id("password")).sendKeys("admin");
+		driver.findElement(By.cssSelector("input[value='ログイン']")).click();
+
+		//ログイン画面の保持を確認
+		assertEquals("ログイン | LMS", driver.getTitle());
 	}
 
 	@Test
@@ -43,6 +71,15 @@ public class Case02 {
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+
+		//エラーメッセージが表示されているか
+		WebElement errorMsg = driver.findElement(By.className("error"));
+
+		// メッセージが画面に表示されているか（CSS等で隠れていないか）
+		assertTrue(errorMsg.isDisplayed(), "エラーメッセージが表示されている必要があります");
+
+		//メッセージの内容は正しいか
+		assertEquals("* ログインに失敗しました。", errorMsg.getText());
 	}
 
 }
